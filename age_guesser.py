01d7528409f612c9bda0bd31b1d3dac998d660a8
Connor Denney <connordenney1031@gmail.com>
2025-01-14 14:21:18 -0700

    made it actually work

--- age_guesser.py
+++ age_guesser.py
@@ -1,22 +1,23 @@
 #age guesser
 import random
 def ageGuess():
-    realage == False
+    realage = False
     notage = []
     print("Hello, I am going to guess your age.\nWhat is your name?")
     name = input()
-    while realage == False:
+    while not realage:
         ages = random.randrange(15, 30)
         if ages in notage:
-            return
-        print("Is your age " + ages +"? \nType Y or N.")
-        yesorno = input()
-        if yesorno == Y or y:
+            continue
+        print("Is your age " + str(ages) +"? \nType Y or N.")
+        yesorno = input().strip().upper()
+        if yesorno == 'Y':
             realage = ages
-        elif yesorno == N or n:
+            break
+        elif yesorno == 'N':
             notage.append(ages)
             print("Rats.")
         else:
-             print('Input not recognised. Type Y or N.')
-    print(name + "is " + realage + 'years old')
-        
+             print('Input not recognised. Type Y or N next time.')
+    print(f"{name} is {realage} years old.")
+ageGuess()
